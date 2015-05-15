fa6236d12aa8aa42a6fed379e2b84d89abead260
Stephen <ssanford32@gmail.com>
2015-05-14 20:03:48 -0600

    This is my first commit

---
+++ HelloWorld.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package helloworld;
+
+/**
+ *
+ * @author Stephen
+ */
+public class HelloWorld {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        HelloWorld hello = new HelloWorld();
+        hello.run();
+    }
+    
+    public void run() {
+        System.out.println("Hello World");
+    }
+}
